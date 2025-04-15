d63fbbf92fd40a66f42f4532127888acd83db1b5
aditya.nehere@comflextech.com <aditya.nehere@comflextech.com>
2025-04-15 10:54:24 +0530

    changes on invoice

--- src/files/app.setting.ts
+++ src/files/app.setting.ts
@@ -7,7 +7,7 @@
 export var AppSetting = {
     // defaultApiUrl: "http://192.168.1.26:1011/",
     // defaultApiUrl: "http://192.168.1.27:2003/", // 27 SERVER
-    defaultApiUrl: "http://localhost:34129/",/*,/**/     
+    defaultApiUrl: "http://localhost:34129/",     
     // defaultApiUrl: "http://103.74.54.211/Cftrackwebapi",
     encryption: false,
     currencyFormatter:"INR"
