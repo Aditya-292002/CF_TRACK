9409a6a9258ef79dcd7315c4c1d080a810cbe357
NKotianComflex <nishit.kotian@comflextech.com>
2025-12-23 14:57:45 +0530

    23-12-2025 UI feedback changes and Binding

--- src/app/Marketing/sales-opportunity-log/sales-opportunity-log.component.ts
+++ src/app/Marketing/sales-opportunity-log/sales-opportunity-log.component.ts
@@ -11,6 +11,7 @@ import { ValidationService } from 'src/app/services/validation.service';
 import { saveAs } from 'file-saver';
 import { v4 as uuidv4 } from 'uuid';
 import { Router } from '@angular/router';
+import { ChangeDetectorRef } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -23,6 +24,7 @@ export class SalesOpportunityLogComponent implements OnInit {
   form: FormGroup;
   spinner: boolean = false;
 
+  oppoDisplay: any;
   isSubmited: boolean = false;
   minDate: any ='';
   maxDate: any ='';
@@ -47,7 +49,9 @@ export class SalesOpportunityLogComponent implements OnInit {
     public validationService: ValidationService,
     private pipeService: PipeService,
     public router: Router,
-    public datepipe: DatePipe) { }
+    public datepipe: DatePipe,
+    public cdr: ChangeDetectorRef) { }
+    
 
     customer_list: Array<any> = [];
     company_list:Array<any>=[];
@@ -55,6 +59,7 @@ export class SalesOpportunityLogComponent implements OnInit {
     opportunity_activity_list: Array<any> =[];
     opportunity_status_list: Array<any> =[];
     opportunity_substatus_list: Array<any> =[];
+    opportunitytype_list: Array<any> =[];
     probability_list: Array<any> =[];
     Nextactivity_list: Array<any> =[];
     log_view_list: Array<any> = []
@@ -101,9 +106,8 @@ export class SalesOpportunityLogComponent implements OnInit {
       this.GetOpportunityLogDetails();
       this.GetLogDetailsView();
     }
-
+    
     $('.selectpicker').selectpicker('refresh').trigger('change');
-
   }
 
 
@@ -152,9 +156,17 @@ export class SalesOpportunityLogComponent implements OnInit {
         this.customer_list = res.customer_list;
         this.company_list = res.company_list;
         this.opportunity_list = res.opportunity_list;
+        // UPDATE Opportunity DISPLAY HERE
+        const code = this.form.get('OPPO_CODE').value;
+        const opp = this.opportunity_list.find(o => o.OPPO_CODE === code);
+        this.oppoDisplay = `${opp.OPPO_CODE} - ${opp.OPPO_NAME}`;
+        this.cdr.detectChanges(); // 🔑 REQUIRED
+        // 
+        this.oppoDisplay = opp ? `${opp.OPPO_CODE} - ${opp.OPPO_NAME}` : '';
         this.opportunity_activity_list = res.opportunity_activity_list;
         this.opportunity_status_list = res.opportunity_status_list;
         this.opportunity_substatus_list = res.opportunity_substatus_list;
+        this.opportunitytype_list = res.opportunitytype_list;
         this.probability_list = res.probability_list;
         this.Nextactivity_list = res.Nextactivity_list;
         this.lead_list = res.lead_list
@@ -225,7 +237,8 @@ export class SalesOpportunityLogComponent implements OnInit {
     console.log(data[0])
     this.form.get('COMPANY_CODE').setValue(data[0].COMPANY_CODE)
     // this.form.get('CUST_CODE').setValue(data[0].CUST_CODE)
-    this.form.get('OPPO_CODE').setValue(data[0].OPPO_CODE)
+    // this.form.get('OPPO_CODE').setValue(data[0].OPPO_CODE)
+    this.form.get('OPPO_CODE').setValue(this.oppoDisplay);
     this.form.get('CONTACT_PERSONS').setValue(data[0].CONTACT_PERSONS)
     this.form.get('REMARKS').setValue(data[0].REMARKS)
     this.form.get('REVISED_ORDERVALUE').setValue(data[0].REVISED_ORDERVALUE)
@@ -729,6 +742,6 @@ getMimeType(extension: string): string {
   input.value = '';
 }
 
+
   
 }
-
